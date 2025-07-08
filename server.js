d87d187c96870e0995b4a798c8376029a1675d88
beshoy-mounir <beshoy_mounir22@yahoo.com>
2025-07-08 19:56:02 +0300

    Fixed smth :p

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const app = require('./app.js');
+
 mongoose
   .connect(
-    'mongodb+srv://user1:besho1234@cluster01.6ik3w.mongodb.net/loginCheck?retryWrites=true&w=majority&appName=Cluster01'
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`
   )
   .then(() => console.log('✅ Database connected successfully'))
   .catch((err) => console.error('❌ Database connection error:', err));
-const port = 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening to port ${port}`));
